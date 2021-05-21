a696927f5ead6543d0c73ec0d7cbb7d82ce48215
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-21 08:32:59 -0600

    Configuring db info

---
+++ db_config.py
@@ -0,0 +1,12 @@
+""" Flask configuration to connect to the database """
+from flask_pymongo import pymongo
+import os
+
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+DB_NAME = os.getenv("DB_NAME")
+
+client = pymongo.MongoClient(f"
+
+                             ")
+db = client.test
