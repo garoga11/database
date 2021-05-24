f0ad5f72f29e48f3d8dae53cbd89f40c8aac1586
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-24 15:31:24 -0600

    Adding database info

--- db_config.py
+++ db_config.py
@@ -6,7 +6,6 @@ DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 DB_NAME = os.getenv("DB_NAME")
 
-client = pymongo.MongoClient(f"
-
-                             ")
-db = client.db-name
+client = pymongo.MongoClient(
+    "mongodb+srv://{DB_USER}:{DB_PASSWORD}@dbexample.kadqv.mongodb.net/{DB_NAME}?retryWrites=true&w=majority")
+db = client.dbExample
