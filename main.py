5e0e58fa31bc5ea07cff8e043cf4756361cbd425
Gabriela Rojas <a6519150007@utch.edu.mx>
2021-05-24 15:30:36 -0600

    Creating post method

--- main.py
+++ main.py
@@ -1,12 +1,43 @@
-from Flask import Flask, jsonify, request
+from flask import Flask, jsonify, request
 from flask_restful import Api, Resource, reqparse, abort
 from flask_pymongo import pymongo
 import db_config as database
 
 app = Flask(__name__)
+api = Api(app)
 
-all_data = database.db.Badge.find()
+#all_data = database.db.Badge.find()
 
+
+class Test(Resource):
+    def get(self):
+        pass
+
+
+class Badge(Resource):
+
+    def post(self):
+        database.db.dbExample.insert_one(
+            {
+                'header_img_url': request.json['header_img_url'],
+                'profile_picture_url': request.json['profile_picture_url'],
+                'age': request.json['age'],
+                'city': request.json['city'],
+                'followers': request.json['followers'],
+                'likes': request.json['likes'],
+                'post': request.json['post'],
+            }
+        )
+
+
+class AllBadge(Resource):
+    """Get all badges"""
+
+    def get(self):
+        pass
+
+
+api.add_resource(Badge, '/new')
 
 if __name__ == '__main__':
     app.run(load_dotenv=True)
